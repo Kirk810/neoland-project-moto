e72f7c6303dc7a76de93fa7ca0bdff5569ec75f7
Adrián <123007421+Kirk810@users.noreply.github.com>
2023-04-24 12:22:05 +0200

    lenguaje

--- neoland-project-moto/src/components/SelectLang.jsx
+++ neoland-project-moto/src/components/SelectLang.jsx
@@ -5,7 +5,12 @@ const SelectLang = () => {
   const { handleLanguage } = useContext(langContext);
   return (
     <select onChange={handleLanguage}>
-      <option value="es-ES">Spanish</option>
+      <option value="es-ES">
+        <img
+          src="https://img2.freepng.es/20180406/qjq/kisspng-flag-of-spain-guess-emoji-spain-5ac7c255c063c2.9498509315230408537881.jpg"
+          alt="Spain"
+        />
+      </option>
       <option value="fr-FR">French</option>
       <option value="en-US">English</option>
     </select>
